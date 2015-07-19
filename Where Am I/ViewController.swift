9a1514697eeb8becad03b8332606c805f706d68b
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-19 21:40:38 +0700

    next to work on: having pin dropped on current location. prevent course & speed negative value.

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    // MARK: - Properties
+    
+    let mapCoordinateZoomLevel: CLLocationDegrees = 0.007
+    var locationManager: CLLocationManager!
+    
+    // MARK: - IBOutlet properties
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: - UIViewController methods override
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.pausesLocationUpdatesAutomatically = true
+        self.locationManager.activityType = CLActivityType.Other
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - CLLocationManagerDelegate methods
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [AnyObject]) {
+        print(locations[0])
+        let userLocation: CLLocation = locations[0] as! CLLocation
+        let userLocationLatitude = userLocation.coordinate.latitude
+        let userLocationLongitude = userLocation.coordinate.longitude
+        let latitudeDeltaZoomLevel = self.mapCoordinateZoomLevel
+        let longitudeDeltaZoomLevel = self.mapCoordinateZoomLevel
+        let areaSpannedByMapRegion = MKCoordinateSpanMake(latitudeDeltaZoomLevel, longitudeDeltaZoomLevel)
+        let geographicalCoordinateStruct = CLLocationCoordinate2DMake(userLocationLatitude, userLocationLongitude)
+        let mapRegionToDisplay = MKCoordinateRegionMake(geographicalCoordinateStruct, areaSpannedByMapRegion)
+        self.mapView.setRegion(mapRegionToDisplay, animated: true)
+        self.latitudeLabel.text = "\(userLocationLatitude)"
+        self.longitudeLabel.text = "\(userLocationLongitude)"
+        self.altitudeLabel.text = "\(userLocation.altitude) meters"
+        self.courseLabel.text = "\(userLocation.course) degrees"
+        self.speedLabel.text = "\(userLocation.speed) meters / second"
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: { (placemarks, error) -> Void in
+            if error != nil {
+                print(error)
+            }
+            else if let poi = CLPlacemark(placemark: placemarks![0] as CLPlacemark) {
+                var subThoroughfare: String = ""
+                if poi.subThoroughfare != nil { subThoroughfare = poi.subThoroughfare }
+                self.addressLabel.text = "\(subThoroughfare) \(poi.thoroughfare) \(poi.subLocality) \n \(poi.subAdministrativeArea), \(poi.administrativeArea) \(poi.postalCode). \(poi.country)."
+            }
+        })
+    }
 }
 
